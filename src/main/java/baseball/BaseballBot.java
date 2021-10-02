abcde182079ff028c62c744eab2125201bf049bc
ch3224bin <ahrocksil@gmail.com>
2021-10-02 13:24:31 +0900

    feat : 게임 시작시 임의의 숫자 세개를 생성한다
    컴퓨터가 가지는 정답을 생성한다.
    1~9 사이의 중복이 없는 세자리 숫자.

--- src/main/java/baseball/BaseballBot.java
+++ src/main/java/baseball/BaseballBot.java
@@ -1,13 +1,36 @@
 package baseball;
 
+import nextstep.utils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BaseballBot {
+    private final static int HIDDEN_NUMBER_COUNT = 3; // 정답 숫자의 자리수
+
     private GameState gameState;
+    private Integer[] hiddenNumber;
 
     public BaseballBot() {
         gameState = GameState.PROGRESS;
+        initHiddenNumber();
+    }
+
+    private void initHiddenNumber() {
+        List<Integer> uniqueNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        hiddenNumber = new Integer[HIDDEN_NUMBER_COUNT];
+        for (int i = 0; i < HIDDEN_NUMBER_COUNT; i++) {
+            int randomIndex = Randoms.pickNumberInRange(0, uniqueNumbers.size() - 1);
+            hiddenNumber[i] = uniqueNumbers.remove(randomIndex);
+        }
     }
 
     public GameState getGameState() {
         return gameState;
+    }
+
+    public Integer[] getHiddenNumber() {
+        return hiddenNumber;
     }
 }
--- src/test/java/baseball/IntegrationTest.java
+++ src/test/java/baseball/IntegrationTest.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import nextstep.test.NSTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,4 +18,30 @@ public class IntegrationTest {
         BaseballBot baseballBot = new BaseballBot();
         assertThat(baseballBot.getGameState()).isEqualTo(GameState.PROGRESS);
     }
+
+    @DisplayName("게임 시작시")
+    @Nested
+    class WhenStart extends NSTest {
+
+        @DisplayName("임의의 숫자 3개를 생성한다")
+        @Test
+        public void testRandomHiddenNumber() {
+            for (int i = 0; i < 100; i++) { // 100번 정도 수행
+                BaseballBot baseballBot = new BaseballBot();
+                Integer[] hiddenNumber = baseballBot.getHiddenNumber();
+
+                // 중복 없는 세자리 숫자 검사
+                assertThat(hiddenNumber).isNotNull();
+                assertThat(hiddenNumber).hasSize(3).allSatisfy(number ->
+                        assertThat(hiddenNumber).filteredOn(n -> Objects.equals(n, number)).hasSize(1)
+                );
+            }
+        }
+
+        @Override
+        public void runMain() {
+
+        }
+    }
+
 }
